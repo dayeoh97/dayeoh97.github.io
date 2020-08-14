4d8fee6162c6ea04b5fe86c5291e296ae9e3e463
Matthew Brown <matthew.e.brown.17@gmail.com>
2020-08-14 15:58:11 -0400

    Upgrade hamburger menu button

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,7 @@
-let menu = document.querySelector('#hamburger_button');
-let cross = document.querySelectorAll('#hamburger_button hr');
-menu.addEventListener('click', () => {
-    cross.forEach(crosshr => {
-        crosshr.classList.toggle('cross');
-    })
-    document.querySelector('#menu').classList.toggle('cross');
-    document.querySelectorAll('#menu a').forEach(menulink => {
-        menulink.classList.toggle('cross');
-    })
-})
+const hamb = document.querySelector('#hamburger_button');
+const menu = document.querySelector('#menu');
+
+hamb.addEventListener('click', () => {
+    hamb.classList.toggle('cross');
+    menu.classList.toggle('cross');
+});
