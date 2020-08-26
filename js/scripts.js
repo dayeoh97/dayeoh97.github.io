816656c1123953d69a780537615e2f24351db5f3
Daye <dhkekdzo@gmail.com>
2020-08-27 00:50:02 +0900

    renewsite

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,5 @@ const hamb = document.querySelector('#hamburger_button');
 const menu = document.querySelector('#menu');
 
 hamb.addEventListener('click', () => {
-    hamb.classList.toggle('cross');
-    menu.classList.toggle('cross');
+    menu.classList.toggle('menu-active');
 });
