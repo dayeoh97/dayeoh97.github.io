6615f6e157042f80ae2b17cc99ddbf590b78645e
Theo Nguyen <neosndt@gmail.com>
2020-08-27 02:41:02 -0400

    Refine navbar and menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
-const hamb = document.querySelector('#hamburger_button');
-const menu = document.querySelector('#menu');
+/*eslint-env browser*/
+let hamButton = document.querySelector('#hamburger-button')
+let shapeLogo = document.querySelector('#logo > div:first-of-type');
+let letterLogo = document.querySelector('#logo > div:last-of-type');
+let menu = document.querySelector("#menu");
 
-hamb.addEventListener('click', () => {
-    menu.classList.toggle('menu-active');
+hamButton.addEventListener('click', () => {
+    hamButton.classList.toggle('menu-switch');
+    shapeLogo.classList.toggle('menu-switch');
+    letterLogo.classList.toggle('menu-switch');
+    menu.classList.toggle('menu-switch');
 });
