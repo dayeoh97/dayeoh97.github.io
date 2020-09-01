bdc10ad55556e966e205aebfcc857333369801c3
Theo Nguyen <neosndt@gmail.com>
2020-09-01 06:15:28 -0400

    Add exploratory transition effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,40 @@
-/*eslint-env browser*/
-let hamButton = document.querySelector('#hamburger-button');
-let logo = document.querySelector('#logo');
-const menu = document.querySelector("#menu");
+const swup = new Swup(); 
 
-//click event for menu
-hamButton.addEventListener('click', () => {
-    hamButton.classList.toggle('menu-switch');
-    logo.classList.toggle('menu-switch');
-    menu.classList.toggle('menu-switch');
-    document.body.classList.toggle('menu-switch');
-    //check if menu is present then disable scrolling
-    if(menu.className === 'menu-switch'){
-        bodyScrollLock.disableBodyScroll(menu, {reserveScrollBarGap: true,});
-    } else {
-        bodyScrollLock.enableBodyScroll(menu);
-    }
-});
+init = () => {
+    const hamButton = document.querySelector('#hamburger-button');
+    const logo = document.querySelector('#logo');
+    const menu = document.querySelector("#menu");
+    menuSwitch = () => {
+        hamButton.classList.toggle('menu-switch');
+        logo.classList.toggle('menu-switch');
+        menu.classList.toggle('menu-switch');
+        document.body.classList.toggle('menu-switch');
+        //check if menu is present then disable scrolling
+        if(menu.className === 'menu-switch'){
+            bodyScrollLock.disableBodyScroll(menu, {reserveScrollBarGap: true,});
+        } else {
+            bodyScrollLock.enableBodyScroll(menu);
+        }
+    };
+    hamButton.addEventListener('click', menuSwitch);
+    const cases = document.querySelectorAll('.case-study');
+    if (cases) {
+        cases.forEach(study => {
+            study.addEventListener('click', () => {
+                study.classList.add('hidden');
+                study.querySelector('img').classList.add('transition-zoom');
+            });
+        });
+    };
+    window.scrollTo(0, 0);
+};
+
+document.addEventListener('DOMContentLoaded', init);
+swup.on('contentReplaced', init);
+
+unload = () => {
+    document.body.classList.remove('menu-switch');
+    bodyScrollLock.enableBodyScroll(menu);
+}
+swup.on('willReplaceContent', unload);
+
